cb4c54c66284bc13fa2f6bfbe2f7fc3b7a6049e4
David Pritchard <davidp100@hotmail.co.uk>
2021-08-20 18:19:27 +0100

    add function to write ascii raster

--- utils.py
+++ utils.py
@@ -1,7 +1,3 @@
-"""Utility functions.
-
-"""
-
 import numpy as np
 import numpy.ma as ma
 
@@ -30,6 +26,22 @@ def read_asc(file_path, data_type=np.float32, return_metadata=True):
         return(arr, nx, ny, xll, yll, dx, dy)
     else:
         return(arr)
+
+def write_asc(array, file_path, format, nx, ny, xll, yll, dx, nodata_value):
+    """Write ascii raster to file."""
+    headers = (
+        'ncols         ' + str(nx) + '\n' +
+        'nrows         ' + str(ny) + '\n' +
+        'xllcorner     ' + str(xll) + '\n' +
+        'yllcorner     ' + str(yll) + '\n' +
+        'cellsize      ' + str(dx) + '\n' +
+        'NODATA_value  ' + str(nodata_value)
+    )
+    if ma.isMaskedArray(array):
+        output_array = array.filled(nodata_value)
+    else:
+        output_array = array
+    np.savetxt(file_path, output_array, fmt=format, header=headers, comments='')
 
 def spatial_mean(val_arr, mask_arr):
     """Calculate spatial mean of values where mask == 1."""
